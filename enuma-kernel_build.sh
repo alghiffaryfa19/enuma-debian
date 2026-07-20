8cf894b05882df51cf1f68050f5204fbe93909df
Fauzan <alghiffaryfa19@gmail.com>
2026-07-20 23:15:42 +0700

    add sdx55m userspace

--- enuma-kernel_build.sh
+++ enuma-kernel_build.sh
@@ -129,33 +129,3 @@ install -m644 modem-userspace/udev/79-mm-sdx55m-net.rules modem-userspace-enuma/
 install -m644 modem-userspace/systemd/m2172-modem.service modem-userspace-enuma/etc/systemd/system/
 
 dpkg-deb --build --root-owner-group modem-userspace-enuma
-
-# ModemManager
-echo "🚀 Building ModemManager"
-git clone https://github.com/meizu-m2172-mainline/ModemManager
-cd ModemManager
-meson setup build --prefix=/usr --buildtype=release -Dsystemdsystemunitdir=/lib/systemd/system -Dwerror=false -Dmbim=true -Dqmi=true -Dqrtr=true -Dpolkit=strict
-ninja -C build
-DESTDIR=$(pwd)/../modemmanager-enuma ninja -C build install
-cd ..
-
-mkdir -p modemmanager-enuma/DEBIAN
-cat << 'EOF' > modemmanager-enuma/DEBIAN/control
-Package: modemmanager-enuma
-Version: 1.20.0-custom
-Architecture: arm64
-Maintainer: alghiffaryfa19
-Conflicts: modemmanager
-Replaces: modemmanager
-Provides: modemmanager
-Description: Custom ModemManager for enuma
-EOF
-
-cat << 'EOF' > modemmanager-enuma/DEBIAN/postinst
-#!/bin/sh
-systemctl daemon-reload || true
-systemctl restart ModemManager.service || true
-EOF
-chmod +x modemmanager-enuma/DEBIAN/postinst
-
-dpkg-deb --build --root-owner-group modemmanager-enuma
