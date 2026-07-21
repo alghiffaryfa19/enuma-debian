fdb761831648092bc8c513325e55be84ee6b713d
Fauzan <alghiffaryfa19@gmail.com>
2026-07-21 17:28:25 +0700

    add alsa builder

--- enuma-kernel_build.sh
+++ enuma-kernel_build.sh
@@ -81,17 +81,7 @@ rm -f ../linux-xiaomi-enuma/lib/modules/*/build ../linux-xiaomi-enuma/lib/module
 
 cd ..
 
-git clone https://github.com/map220v/alsa-ucm-conf
-mkdir -p alsa-xiaomi-enuma/usr/share/alsa/ucm2/Xiaomi/enuma
-cp -r alsa-ucm-conf/ucm2 alsa-xiaomi-enuma/usr/share/alsa/
-install -Dm644 enuma.conf alsa-xiaomi-enuma/usr/share/alsa/ucm2/Xiaomi/enuma/enuma.conf
-install -Dm644 HiFi.conf alsa-xiaomi-enuma/usr/share/alsa/ucm2/Xiaomi/enuma/HiFi.conf
-
-mkdir -p alsa-xiaomi-enuma/usr/share/alsa/ucm2/conf.d/sm8250
-ln -s "../../Xiaomi/enuma/enuma.conf" "alsa-xiaomi-enuma/usr/share/alsa/ucm2/conf.d/sm8250/Xiaomi Mi Pad 5 Pro 5G.conf"
-
 dpkg-deb --build --root-owner-group linux-xiaomi-enuma
-dpkg-deb --build --root-owner-group alsa-xiaomi-enuma
 
 # modem-userspace
 echo "🚀 Building modem-userspace"
